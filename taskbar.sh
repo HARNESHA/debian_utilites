d40a8f7dfeb22d9c3abe7f39cc2974b70e07be3f
JAY HARNESHA <82324338+HARNESHA@users.noreply.github.com>
2022-03-01 07:27:45 +0530

    Add files via upload
    taskbar custmization file updatred

---
+++ taskbar.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 20
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0
