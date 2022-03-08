40150dbfcf2c29f8883098c900f361f5d7cc5004
JAY HARNESHA <82324338+HARNESHA@users.noreply.github.com>
2022-03-08 23:17:15 +0530

    Update taskbar.sh

--- taskbar.sh
+++ taskbar.sh
@@ -3,3 +3,4 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 20
 gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
